1c1fe8b6e10573fa58f706000b992c306c222528
Youngdo <imyoungdocho@gmail.com>
2018-09-11 11:02:03 +0900

    reformat transaction/__init__.py

--- app/transaction/__init__.py
+++ app/transaction/__init__.py
@@ -5,31 +5,38 @@ from app.node import key
 from app import storage
 from app.communicator import sender
 
+
 # todo implement functions below
 def add_transaction(tx):
-	return
+    return
+
 
 def get_transactions():
-	# return mandatory
-	return
+    # return mandatory
+    return
+
 
 def count():
-	# return mandatory
-	return
+    # return mandatory
+    return
+
 
 def remove_all():
-	return
+    return
+
 
 # important!
 def create_tx(msg):
-	# return mandatory
-	return
+    # return mandatory
+    return
+
 
 # distribute Tx to all
 def send_tx(tx):
-	return
+    return
+
 
 def validate_tx(pub_key, signature, message):
-	#전자서명을 검증함
-	if key.verify_signature(pub_key, signature, message):
-		return True
+    # 전자서명을 검증함
+    if key.verify_signature(pub_key, signature, message):
+        return True
